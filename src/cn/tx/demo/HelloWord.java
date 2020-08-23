cda5421ca9a220a03aa5e0df4b84988fff856233
lxie2020822 <70052160+lxie2020822@users.noreply.github.com>
2020-08-23 22:26:29 +0800

    Update HelloWord.java

--- src/cn/tx/demo/HelloWord.java
+++ src/cn/tx/demo/HelloWord.java
@@ -5,14 +5,17 @@ public class HelloWord {
     public static void main(String[] args) {
         int a=10;
         int b=5;
-        int c=a+b;
-        System.out.println("helloword");
-        System.out.println("helloword");
-        System.out.println("helloword");
-        System.out.println("helloword");
-        System.out.println("helloword");
-        System.out.println("helloword");
-        System.out.println("helloword");
+        int c=a++;
+        int d=c++;
+        int e=a+b;
+        int f=c+d;
+        System.out.println("a");
+        System.out.println("a");
+        System.out.println("b");
+        System.out.println("c");
+        System.out.println("d");
+        System.out.println("e");
+        System.out.println("f");
         System.out.println("helloword");
         System.out.println("helloword");
         System.out.println("helloword");
