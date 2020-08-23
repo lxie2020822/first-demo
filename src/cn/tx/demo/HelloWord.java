b4d9b5482ef31cebb67cacf010ec1f1342f68fba
lxie2020822 <xl731269941@163.com>
2020-08-23 19:34:45 +0800

    测试提交

---
+++ src/cn/tx/demo/HelloWord.java
@@ -0,0 +1,21 @@
+package cn.tx.demo;
+
+
+public class HelloWord {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        int c=a+b;
+        System.out.println("helloword");
+        System.out.println("helloword");
+        System.out.println("helloword");
+        System.out.println("helloword");
+        System.out.println("helloword");
+        System.out.println("helloword");
+        System.out.println("helloword");
+        System.out.println("helloword");
+        System.out.println("helloword");
+        System.out.println("helloword");
+        System.out.println("helloword");
+    }
+}
