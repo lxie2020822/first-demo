b45c0f56e762c0ad2772b369508d8e39ef070802
lxie2020822 <xl731269941@163.com>
2020-08-23 22:15:55 +0800

    测试提交

---
+++ src/cn/tx/demo/Person.java
@@ -0,0 +1,51 @@
+package cn.tx.demo;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 6278277205954456066L;
+
+    private int id;
+
+    private String name;
+
+    private int age;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
